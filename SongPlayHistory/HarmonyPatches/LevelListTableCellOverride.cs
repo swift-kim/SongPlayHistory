b8de500bee7eac7f4d0b281f63007f6249eb1d43
Swift Kim <swift.kim@samsung.com>
2020-02-10 20:34:47 +0900

    Add a skeleton of LevelListTableCellOverride

---
+++ SongPlayHistory/HarmonyPatches/LevelListTableCellOverride.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Harmony;
+using TMPro;
+using UnityEngine.UI;
+
+namespace SongPlayHistory.HarmonyPatches
+{
+    [HarmonyPatch(typeof(LevelListTableCell))]
+    [HarmonyPatch("SetDataFromLevelAsync", new Type[] { typeof(IPreviewBeatmapLevel), typeof(bool) })]
+    public class LevelListTableCellOverride
+    {
+        private Image _thumbUp;
+        private Image _thumbDown;
+
+        [HarmonyAfter(new string[] { "com.kyle1413.BeatSaber.SongCore" })]
+        static void Postfix(LevelListTableCell __instance, IPreviewBeatmapLevel level, bool isFavorite,
+            ref TextMeshProUGUI ____songNameText, ref TextMeshProUGUI ____authorText)
+        {
+
+        }
+    }
+}
