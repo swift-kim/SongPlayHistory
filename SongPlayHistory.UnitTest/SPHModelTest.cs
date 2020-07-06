69d0eb10fc8655c81554bb1358068457b2676291
Swift Kim <swift.kim@samsung.com>
2020-07-06 21:49:27 +0900

    Enable logging for tests

--- SongPlayHistory.UnitTest/SPHModelTest.cs
+++ SongPlayHistory.UnitTest/SPHModelTest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SongPlayHistory.UnitTest
 {
@@ -10,8 +11,10 @@ namespace SongPlayHistory.UnitTest
     {
         public readonly string UserDataDir = Path.Combine(Environment.CurrentDirectory, "UserData");
 
-        public SPHModelTest()
+        public SPHModelTest(ITestOutputHelper output)
         {
+            Plugin.Log = new TestLogger() { Output = output };
+
             if (Directory.Exists(UserDataDir))
             {
                 Directory.Delete(UserDataDir, true);
@@ -33,15 +36,17 @@ namespace SongPlayHistory.UnitTest
         {
             var records = new Dictionary<string, IList<Record>>();
             records.Add("level1", new List<Record> { new Record() });
-            records.Add("level2", new List<Record> { new Record() });
+            records.Add("level2", new List<Record> { new Record(), new Record() });
 
             var serialized = JsonConvert.SerializeObject(records);
-            File.WriteAllText(SPHModel.DataFile, $"{{\"Scores\":{serialized}}}");
+            File.WriteAllText(SPHModel.DataFile, serialized);
 
-            SPHModel.InitializeRecords(); // Why NullReferenceException?
+            SPHModel.InitializeRecords();
 
             Assert.True(File.Exists(SPHModel.DataFile));
             Assert.True(SPHModel.Records.Count == 2);
+            Assert.True(SPHModel.Records["level1"].Count == 1);
+            Assert.True(SPHModel.Records["level2"].Count == 2);
         }
     }
 }
---
+++ SongPlayHistory.UnitTest/TestLogger.cs
@@ -0,0 +1,15 @@
+﻿using IPA.Logging;
+using Xunit.Abstractions;
+
+namespace SongPlayHistory.UnitTest
+{
+    public class TestLogger : Logger
+    {
+        public ITestOutputHelper Output { get; set; }
+
+        public override void Log(Level level, string message)
+        {
+            Output?.WriteLine(message);
+        }
+    }
+}
